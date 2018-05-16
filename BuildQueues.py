c4fc1d2d777e830cb2352ef8ba095735aa69e68f
Emil Aliyev <33892185+EmilAliyev@users.noreply.github.com>
2018-05-15 17:07:06 -0700

    Add files via upload

---
+++ BuildQueues.py
@@ -0,0 +1,59 @@
+#Building Queue
+class BuildingQueue:
+    #Build order:
+    #Hatchery
+    #Spawning Pool
+    #Spine crawler
+    #Extractor
+    #Roach Warren
+    #Extractor x3
+    #Lair
+    #Hydralisk Den
+    #Spore Crawler
+    #Hatchery
+
+    def _init_(self):
+        #Set build order
+
+    def dequeue(self):
+        #dequeue
+
+    def enqueue(self):
+        #enqueue
+
+#Unit Queue
+
+class UnitQueue:
+    #Build order:
+    #Queen
+    #Zerglings
+    #Roaches
+    #Hydralisks
+
+    def _init_(self):
+        #Set build order
+
+    def dequeue(self):
+        #dequeue
+
+    def enqueue(self):
+        #enqueue
+
+#Research Queue
+
+class ResearchQueue:
+    #Order:
+    #Metabolic Boost
+    #Glial reconstitution
+    #Zerg Missile Attacks level 1
+    #Grooved Spines
+    #Zerg Missile Attacks level 2
+    def _init_(self):
+        #Set build order
+
+    def dequeue(self):
+        #dequeue
+
+    def enqueue(self):
+        #enqueue
+
