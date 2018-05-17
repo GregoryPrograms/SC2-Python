6e7030a5498c2a795566f1e4aaca2725b1d84e1f
whloo-rebel-scum <whloo.rebel.scum@outlook.com>
2018-05-17 08:42:45 -0700

    added macros for the BuildQueues

--- BuildQueues.py
+++ BuildQueues.py
@@ -1,7 +1,24 @@
+
+from pysc2.agents import base_agent
+from pysc2.lib import actions
+from pysc2.lib import features
+
+# Building Macros
+_BUILD_HATCHERY = actions.FUNCTIONS.Build_Hatchery_screen.id
+_BUILD_SPAWNING_POOL = actions.FUNCTIONS.Build_SpawningPool_screen.id
+_BUILD_SPINE_CRAWLER = actions.FUNCTIONS.Build_SpineCrawler_screen.id
+_BUILD_EXTRACTOR = actions.FUNCTIONS.Build_Extractor_screen_screen.id
+_BUILD_ROACH_WARREN = actions.FUNCTIONS.Build_RoachWarren_screen.id
+_BUILD_LAIR = actions.FUNCTIONS.Morph_Lair_quick.id
+    #from Hatchery
+_BUILD_HYDRALISK_DEN = actions.FUNCTIONS.Build_HydraliskDen_screen.id
+_BUILD_SPORE_CRAWLER = actions.FUNCTIONS.Build_SporeCrawler_screen.id
+
+
 # Building Queue
 class BuildingQueue:
     # Build order:
-    # Hatchery
+    # Hatchery: return Actions.FunctionCall('Build_Hatchery_screen', cmd_screen, 1152)
     # Spawning Pool
     # Spine crawler
     # Extractor
@@ -22,6 +39,12 @@ class BuildingQueue:
         """enqueue"""
 
 
+# Unit Macros
+_TRAIN_QUEEN = actions.FUNCTIONS.Train_Queen_quick.id
+_TRAIN_ZERGLING = actions.FUNCTIONS.Train_Zergling_quick.id
+_TRAIN_ROACH = actions.FUNCTIONS.Train_Roach_quick.id
+_TRAIN_HYDRALISK = actions.FUNCTIONS.Train_Hydralisk_quick.id
+
 # Unit Queue
 
 class UnitQueue:
@@ -40,6 +63,15 @@ class UnitQueue:
     def enqueue(self):
         """enqueue"""
 
+
+# Research Macros
+_RESEARCH_METABOLIC_BOOST = actions.FUNCTIONS.Research_ZerglingMetabolicBoost_quick.id
+_RESEARCH_GLIAL = actions.FUNCTIONS.Research_GlialRegeneration_quick.id
+    #closest thing I could find to glial reconstitution in the actions list
+_RESEARCH_ZERG_MISSILE_WEAPONS = actions.FUNCTIONS.Research_ZergMissileWeapons_quick.id
+_RESEARCH_ZERG_MISSILE_LVL1 = actions.FUNCTIONS.Research_ZergMissileWeaponsLevel1_quick.id
+_RESEARCH_ZERG_MISSILE_LVL2 = actions.FUNCTIONS.Research_ZergMissileWeaponsLevel2_quick.id
+_RESEARCH_GROOVED_SPINES = actions.FUNCTIONS.Research_GroovedSpines_quick.id
 
 # Research Queue
 
